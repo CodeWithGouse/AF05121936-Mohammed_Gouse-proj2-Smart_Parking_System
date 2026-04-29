7ae6dc2f1779f031c17e8a14932fb1694b0ae8cd
Mohammed Gouse <mgouse539@gmail.com>
2026-04-29 22:05:41 +0530

    Update DBConnection.java

--- Smart_Parking_System/src/DBConnection.java
+++ Smart_Parking_System/src/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/parking_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "sayed";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         try {
